7a66b7ba1fd5237e80f22ca61e72f72bf192b279
LHZ <71952870@qq.com>
2023-08-26 07:55:04 +0930

    add game attempts

--- guess_game.py
+++ guess_game.py
@@ -1,12 +1,19 @@
 import random
 
+
 class GuessGame:
-    
+
     def __init__(self):
+        """
+        Generate a random four-digit number as the secret number, and initialize the number of attempts.
+        """
         self.secret_number = self.generate_random_number()
         self.attempts = 0
 
     def generate_random_number(self):
+        """
+        Generate a random four-digit number as the secret number.
+        """
         return random.randint(1000, 9999)
 
     def check_guess(self, guess):
@@ -17,45 +24,64 @@ class GuessGame:
             return self.compare_numbers(guess)
 
     def compare_numbers(self, guess):
+        """
+        Compare the player's guess with the secret number and generate a feedback message.
+        """
         secret_str = str(self.secret_number)
         guess_str = str(guess)
-        circles = 0
-        crosses = 0
+        result = [''] * 4
+
         for i in range(4):
             if guess_str[i] == secret_str[i]:
-                circles += 1
+                # If the digit is correct and in the correct position, mark it as 'O'
+                result[i] = 'O'
             elif guess_str[i] in secret_str:
-                crosses += 1
-        return f"{circles} circles and {crosses} crosses"
+                # If the digit is correct and in the correct position, mark it as 'X'
+                result[i] = 'X'
+        return ''.join(result)
+
+    def display_game_result(self):
+        """
+        Display the number of attempts made by the player and ask if they want to play again.
+        """
+        print(f"Total attempts: {self.attempts}")
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        return play_again == 'yes'
+
 
 def main():
     play_again = True
-
+    # Start the game loop
     while play_again:
         game = GuessGame()
         print("Welcome to the Guess the Number game!")
         print("Try to guess the 4-digit number.")
-        
+        # Start the guessing loop for the current game
         while True:
             guess = input("Enter your guess (or 'q' to quit): ")
-            
+
             if guess.lower() == 'q':
                 print(f"The secret number was {game.secret_number}.")
                 break
-            
+
             try:
                 guess = int(guess)
                 if 1000 <= guess <= 9999:
+                    # Check the player's guess and get feedback
                     result = game.check_guess(guess)
                     print(result)
+                    # If the player wins, exit the guessing loop
                     if result.startswith("Congratulations!"):
                         break
                 else:
                     print("Please enter a 4-digit number.")
             except ValueError:
                 print("Invalid input. Please enter a 4-digit number or 'q' to quit.")
-        
-        play_again = input("Do you want to play again? (yes/no): ").lower() == 'yes'
+        # Display the game result and ask if the player wants to play again
+        play_again = game.display_game_result()
+        if play_again != 'yes':
+            break
+
 
 if __name__ == "__main__":
     main()
--- guess_game_tests.py
+++ guess_game_tests.py
@@ -1,31 +1,45 @@
 import unittest
 from guess_game import GuessGame
 
+
 class TestGuessGame(unittest.TestCase):
-    
+
     def test_generate_random_number(self):
         game = GuessGame()
         number = game.generate_random_number()
+        # Check if the generated number is an integer
         self.assertTrue(isinstance(number, int))
+        # Check if the number is within the valid range
         self.assertTrue(1000 <= number <= 9999)
 
-    def test_guess_correct_number(self):
+    def test_compare_numbers(self):
+        """
+        Test the 'compare_numbers' method of the GuessGame class.
+        It checks if the feedback message is correctly generated based on the player's guess.
+        """
         game = GuessGame()
         game.secret_number = 1234
-        result = game.check_guess(1234)
-        self.assertEqual(result, "Congratulations! You guessed the correct number in 1 attempts.")
 
-    def test_guess_incorrect_number(self):
-        game = GuessGame()
-        game.secret_number = 1234
-        result = game.check_guess(5678)
-        self.assertEqual(result, "0 circles and 0 crosses")
+        # Test when all digits are correct and in the correct position
+        result1 = game.compare_numbers(1234)
+        # Expected feedback: 'OOOO'
+        self.assertEqual(result1, "OOOO")
 
-    def test_guess_partial_correct_number(self):
-        game = GuessGame()
-        game.secret_number = 1234
-        result = game.check_guess(1243)
-        self.assertEqual(result, "2 circles and 2 crosses")
+        # Test when all digits are correct but in the wrong position
+        result2 = game.compare_numbers(4321)
+        # Expected feedback: 'XXXX'
+        self.assertEqual(result2, "XXXX")
+
+        # Test when some digits are correct and in the correct position, some are correct but in the wrong position
+        result3 = game.compare_numbers(1243)
+        # Expected feedback: 'OOXX'
+        self.assertEqual(result3, "OOXX")
+
+        # Test when none of the digits are correct
+        result4 = game.compare_numbers(5678)
+        # Expected feedback: ''
+        self.assertEqual(result4, "")
+
 
 if __name__ == '__main__':
     unittest.main()
